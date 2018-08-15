5d2bf0ca2bc9d864164595dcca76dec86afd2f8e
Alexey Ragozin <alexey.ragozin@gmail.com>
2018-08-15 14:32:49 +0300

    Generic thread pseudo state inferer

---
+++ sjk-hflame/src/main/java/org/gridkit/jvmtool/hflame/GenericTerminatingStackElement.java
@@ -0,0 +1,32 @@
+package org.gridkit.jvmtool.hflame;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.gridkit.jvmtool.stacktrace.GenericStackElement;
+
+public class GenericTerminatingStackElement implements GenericStackElement {
+
+	private final String caption;
+	private final Map<String, String> props;
+	
+	public GenericTerminatingStackElement(String caption) {
+		this.caption = caption;
+		this.props = Collections.emptyMap();
+	}
+	
+	public GenericTerminatingStackElement(String caption, Map<String, String> props) {
+		this.caption = caption;
+		this.props = Collections.unmodifiableMap(new LinkedHashMap<String, String>(props));
+	}
+	
+	public Map<String, String> props() {
+		return props;
+	}
+	
+	@Override
+	public String toString() {
+		return caption;
+	}
+}
---
+++ sjk-hflame/src/main/java/org/gridkit/jvmtool/hflame/TraceMapper.java
@@ -0,0 +1,10 @@
+package org.gridkit.jvmtool.hflame;
+
+import org.gridkit.jvmtool.stacktrace.GenericStackElement;
+import org.gridkit.jvmtool.stacktrace.ThreadSnapshot;
+
+public interface TraceMapper {
+	
+	public GenericStackElement generateTraceTerminator(ThreadSnapshot snap);
+	
+}
